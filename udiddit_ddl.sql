0c4fce09c4be25861ea76730cc75ca7ddf70ad05
Alexandra Baturina <alexandra.v.baturina@gmail.com>
2024-12-21 21:20:44 -0800

    chore: create the topics table

--- udiddit_ddl.sql
+++ udiddit_ddl.sql
@@ -11,3 +11,15 @@ CREATE INDEX idx_users_last_logon_date ON users (last_logon_date DESC);
 
 -- Find a user by their username
 CREATE UNIQUE INDEX idx_users_username ON users (LOWER(username));
+
+
+-- Create the topics table
+CREATE TABLE topics (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  description VARCHAR(500) DEFAULT NULL,
+  CONSTRAINT topic_name_length CHECK (LENGTH(TRIM(name)) > 0)
+);
+
+-- Find a topic ty its name
+CREATE UNIQUE INDEX idx_topics_name ON topics (LOWER(name) ASC);
