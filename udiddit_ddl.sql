b9cd7832a8d04618f1e97922675eea55fe0e3229
Alexandra Baturina <alexandra.v.baturina@gmail.com>
2024-12-24 19:14:03 -0800

    chore: create the posts table

--- udiddit_ddl.sql
+++ udiddit_ddl.sql
@@ -23,3 +23,39 @@ CREATE TABLE topics (
 
 -- Find a topic ty its name
 CREATE UNIQUE INDEX idx_topics_name ON topics (LOWER(name) ASC);
+
+
+-- Create the posts table
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  topic_id INTEGER,
+  post_date TIMESTAMP,
+  user_id INTEGER,
+  url VARCHAR(500) DEFAULT NULL,
+  text_content VARCHAR(1000) DEFAULT NULL,
+  CONSTRAINT fk_valid_topic FOREIGN KEY (topic_id)
+    REFERENCES topics(id) ON DELETE CASCADE,
+  CONSTRAINT fk_valid_user FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE SET NULL,
+  CONSTRAINT url_or_text CHECK (
+    (url IS NULL OR text_content IS NULL)
+    AND NOT
+    (url IS NULL AND text_content IS NULL)
+  )
+);
+
+-- List all users who haven't created any posts
+CREATE INDEX idx_users_without_posts ON posts (user_id);
+
+-- List all topics that don't have any posts
+CREATE INDEX idx_topics_without_posts ON posts (topic_id);
+
+-- List the latest 20 posts for a given topic
+CREATE INDEX idx_latest_posts_for_topic ON posts (topic_id, post_date DESC);
+
+-- List the lates 20 posts made by a given_user
+CREATE INDEX idx_latest_posts_by_user ON posts (user_id, post_date DESC);
+
+--Find all posts that link to a specific URL
+CREATE INDEX idx_post_to_url ON posts (url);
