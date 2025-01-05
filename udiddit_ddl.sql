63e5f50738fbc915f0a7e3a53c1bde98981b23ff
Alexandra Baturina <alexandra.v.baturina@gmail.com>
2025-01-04 18:32:41 -0800

    chore: remove redundant constraints

--- udiddit_ddl.sql
+++ udiddit_ddl.sql
@@ -1,7 +1,7 @@
 -- Create the users table
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(25) NOT NULL,
+  username VARCHAR(25) UNIQUE NOT NULL,
   last_logon_date DATE,
   CONSTRAINT username_length CHECK (LENGTH(TRIM(username)) > 0)
 );
@@ -9,20 +9,14 @@ CREATE TABLE users (
 -- List all users who haven't logged in in the last year
 CREATE INDEX idx_users_last_logon_date ON users (last_logon_date DESC);
 
--- Find a user by their username
-CREATE UNIQUE INDEX idx_users_username ON users (LOWER(username));
-
 
 -- Create the topics table
 CREATE TABLE topics (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) UNIQUE NOT NULL,
   description VARCHAR(500) DEFAULT NULL,
   CONSTRAINT topic_name_length CHECK (LENGTH(TRIM(name)) > 0)
 );
-
--- Find a topic ty its name
-CREATE UNIQUE INDEX idx_topics_name ON topics (LOWER(name) ASC);
 
 
 -- Create the posts table
